3d3d5336f4dd17576cc8ce0d5a347d89a4ccddc6
BrajaTannady <brajatannady11@gmail.com>
2020-04-03 10:05:35 +0700

    first commit

---
+++ src/main/java/com/example/SpringCommandPattern/SpringCommandPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCommandPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommandPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommandPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringCommandPattern/SpringCommandPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCommandPattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommandPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
